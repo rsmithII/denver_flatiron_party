f4fdec4a102ec8822d25c545d940163010df838b
Katie Bittner <katiebittner1@gmail.com>
2020-10-21 13:18:44 -0400

    adding drinks list

--- frontend/index.js
+++ frontend/index.js
@@ -1,8 +1,29 @@
-const baseURL = 'http://localhost:3001'
+const baseURL = 'http://localhost:3000'
+const params = new URLSearchParams(window.location.search)
 
-fetch(baseURL)
-    .then(parseJSON)
-    .then(result => console.log())
+getDrinkList()
+
+function getDrinkList(){
+
+  drinks = ["beer","wine","liquor","mixer"]
+  drinks.forEach(drink => {
+    getDrinks(drink)
+  })
+
+  function getDrinks(drink){
+    const currentDrink = drink
+    const drinks = fetch(baseURL+`/${drink}s`)
+      .then(response => parseJSON(response))
+      .then(drinks => {
+        const drinksList = document.querySelector(`#${drink}s`)
+        drinks.forEach( drink => {
+          const li = document.createElement("li")
+          li.textContent = `${drink.name}`
+          drinksList.appendChild(li)
+        })
+      })
+  }
+}
 
 function parseJSON(response) {
     return response.json()
